d01ac422e413b99d907dae121dd1055aa474495a
123 <123@gmail.com>
2021-03-28 02:12:26 +0800

    add time

--- dice_roller.py
+++ dice_roller.py
@@ -3,10 +3,9 @@ import time
 
 def main():
 	dice_rolls = int(input("你想掷几次骰子？"))
-	dice_size = int(input("骰子最高设置几点？"))
 	dice_sum = 0
 	for i in range(0, dice_rolls):
-		roll = random.randint(1, dice_size)
+		roll = random.randint(1, 6)
 		dice_sum += roll
 		if roll == 1:
 			print(f'差劲，掷到 {roll} 点了。。。')
@@ -14,7 +13,7 @@ def main():
 			print(f'不错，掷到 {roll} 点了!')
 		else:
 			print(f'你掷到了 {roll} 点')
-		time.sleep(0.5)
+		time.sleep(0.2)
 	print(f"你一共掷到 {dice_sum} 点！")
 
 if __name__== "__main__":
